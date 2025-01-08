d8d42af809bdf38da0c7300913105a1ff81baff8
rui <ryxxastro@outlook.com.com>
2025-01-08 10:07:35 +0800

    minor

--- GRcmb/GRcmb_2.py
+++ GRcmb/GRcmb_2.py
@@ -22,7 +22,7 @@ inputns = inputdata[4]
  
  
 t0 = timeit.time.time()
-  
+fontsize1 = 20  
  
    
 
@@ -85,8 +85,8 @@ class clcalc(object):
 
    def intgk(self, lnum ):      
       kfinnpt=2000
-      if lnum>300:
-         kfinnpt = 1500
+      #if lnum>300:
+         #kfinnpt = 1500
       knumfinset=np.linspace(self.kmin, self.kmax, kfinnpt )
       transf = np.zeros(kfinnpt)
       testyset = np.zeros(kfinnpt)
